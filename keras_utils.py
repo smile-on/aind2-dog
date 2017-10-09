12e85dde0a1a9bd9d626a011b83b6e9cb0a48c77
Slav Kochepasov <vkochepasov@gmail.com>
2017-10-09 20:01:25 +0000

    drafted step 5, 6.

---
+++ keras_utils.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def draw_error_surface(history):
+    ''' usage: 
+    fit_stats = model.fit()
+    draw_error_surface(fit_stats.history)
+    '''
+    # summarize history for accuracy
+    plt.plot(history['acc'])
+    plt.plot(history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
